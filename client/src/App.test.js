7dac9e85231bdb51bc646168faa1c46f2760dcca
Samik Malhotra <samikmalhotra66@gmail.com>
2022-01-23 03:44:49 +0530

    added dashboard

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { shallow } from 'enzyme/build'
+import App from './App'
+import Dashboard from './views/dashboard/Dashboard.js'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it('mounts App without crashing', () => {
+  const wrapper = shallow(<App />)
+  wrapper.unmount()
+})
+
+it('mounts Dashboard without crashing', () => {
+  const wrapper = shallow(<Dashboard />)
+  wrapper.unmount()
+})
